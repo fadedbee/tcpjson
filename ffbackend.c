94dc9ab1c623763b8ebdc56ce6cb13fb92350278
fadedbee <github@fadedbee.com>
2017-12-06 20:02:34 +0000

    ffbackend getting data from ISAM file

--- ffbackend.c
+++ ffbackend.c
@@ -16,16 +16,56 @@ EXTERN RESRC *testPanel,*testDD;
 
 int ffbackend()
 {
-	sys_header("niv_test","niv_test");
+ 	sysHeaderDaemon();  
 	printf("ffbackend starting...\r\n");
 	
 	testPanel = ffopenrsc("/home/multisys/source/resources/test.pnl");
 	testDD = ffopenrsc("/home/multisys/source/resources/niv_test.rsc");	
 
+        printf("pre-ISAM C\n");
 	ffisam(0,SO,NUM_OPEN_FILES,512);
 	int status = ffisam(STOCK_BNR,OF,testDD,"niv_test","",&STOCK,NULLP) && iserrno;
-	if (status) exit(1);
+	if (status) {
+                printf("ISAM Error C\n");
+		exit(1);
+        }
 	
+
+/*
+        printf("pre-ISAM A\n");
+        ASSS(STOCK.PCODE,"1234");
+        ASSS(STOCK.DESC, "TEST DATA2");
+        ASSI(STOCK.BRAND_ID,1);
+        ASSS(STOCK.MFG_P_NO,"TEST_MFG_PART_NO2");
+        ASSL(STOCK.LIST_PR,2.00L);
+        ASSL(STOCK.SELL_PR,3.00L);
+        ASSI(STOCK.UNIT_ID,1);
+        ASSS(STOCK.COLOUR,"YLW");
+        ASSS(STOCK.P_GROUP,"TEST");
+        ASSL(STOCK.LAST_PO,291117);
+
+        if(ffisam(STOCK_BNR,IR)) {
+                printf("ISAM Error A\n");
+		exit(1);
+        }
+*/
+
+
+        printf("pre-ISAM B\n");
+        ASSS(STOCK.PCODE,"1234");
+        printf("pre-ISAM B1\n");
+        if(!ffisam(STOCK_BNR,RK,1,0)) {
+                printf("post-ISAM B2\n");
+                char description[sizeof(STOCK.DESC)+1];
+                GETSTR(description,STOCK.DESC);
+                printf("post-ISAM B3\n");
+                printf("%s\n",description);
+        } else {
+                printf("ISAM Error B\n");
+		exit(1);
+        }
+        printf("post-ISAM B4\n");
+
 	ffisam(STOCK_BNR,CF);
 
 	printf("ffbackend finishing...\r\n");
